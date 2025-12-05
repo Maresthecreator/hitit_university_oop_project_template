9f4f7e7e6b55617d529afac7d51886e9efacf3ba
YUSUFCAN YILMAZ <brtbrt14@gmail.com>
2025-12-05 13:10:02 +0300

    main.py yi athlete modülünün demo suna bağladım

--- app/modules/module_athlete/demo.py
+++ app/modules/module_athlete/demo.py
@@ -12,7 +12,7 @@ def run_demo() -> None:
 
     athlete1 = ProfessionalAthlete(
     athlete_id=1,
-    name='<Yusuf Profesyonel Atlet',
+    name='Yusuf Profesyonel Atlet',
     age=27,
     sport_branch=SportBranch.Football,
     status=AthleteStatus.Active,
@@ -22,29 +22,61 @@ def run_demo() -> None:
 
     athlete2 = AmateurAthlete(
     athlete_id=2,
-    name="Ayse Amatör Atlet",
+    name="Ayşe Amatör Atlet",
     age=21,
     sport_branch=SportBranch.Basketball,
     status=AthleteStatus.Active,
     base_fee=350.0,
     has_student_discount=True,
     )
+
     athlete3 = YouthAthlete(
     athlete_id=3,
-    name='<Yusuf Youth',
-    age=28,
+    name='Ilgın Genç Atlet',
+    age=17,
     sport_branch=SportBranch.Voleyball,
     status=AthleteStatus.Injured,
-    school_name='<Anadolu Lisesi',
+    school_name='Erzurum Proje Lisesi',
     parent_contact='Mehmet 0512 345 67 89',
     )
-    for athlete in [athlete1, athlete2, athlete3]:
+
+    athlete4 = AmateurAthlete(
+    athlete_id=4,
+    name="Can Amatör Atlet ",
+    age=23,
+    sport_branch=SportBranch.Swimming,
+    status=AthleteStatus.Suspended,
+    base_fee=560.0,
+    has_student_discount=False,
+    )
+
+    athlete5 = YouthAthlete(
+    athlete_id=5,
+    name="Yılmaz Genç Atlet",
+    age=16,
+    sport_branch=SportBranch.Golf,
+    status=AthleteStatus.Active,
+    school_name="Atatürk Fen Lisesi",
+    parent_contact='Eylül 0555 345 67 89',
+    )
+    athlete6 = ProfessionalAthlete(
+    athlete_id=6,
+    name="Hakan Profesyonel Atlet",
+    age=51,
+    sport_branch=SportBranch.Tennis,
+    status=AthleteStatus.Retired,
+    salary=0,
+    contract_years=0,
+    )
+
+
+    for athlete in [athlete1, athlete2, athlete3, athlete4, athlete5, athlete6]:
         repo.add(athlete)
 
     print("---Sporcularımız---")
     for athlete in repo.list_all():
         print("Aidat:", athlete.calculate_membership_fee())
-        print("-" * 40)
+        print("-" * 50)
 
     print("---Sadece Futbolcular---")
     for athlete in repo.filter_by_branch(SportBranch.Football):
@@ -58,6 +90,18 @@ def run_demo() -> None:
     for athlete in repo.filter_by_branch(SportBranch.Voleyball):
         print(athlete.get_profile_summary())
 
+    print("---Sadece Tenisciler---")
+    for athlete in repo.filter_by_branch(SportBranch.Tennis):
+        print(athlete.get_profile_summary())
+
+    print("---Sadece Yüzücüler---")
+    for athlete in repo.filter_by_branch(SportBranch.Swimming):
+        print(athlete.get_profile_summary())
+
+    print("---Sadece Golfcü---")
+    for athlete in repo.filter_by_branch(SportBranch.Golf):
+        print(athlete.get_profile_summary())
+
     print("---Aktif Sporcular---")
     for athlete in repo.filter_by_status(AthleteStatus.Active):
         print(athlete.get_profile_summary())
@@ -65,3 +109,15 @@ def run_demo() -> None:
     print("---Sakat Sporcular---")
     for athlete in repo.filter_by_status(AthleteStatus.Injured):
         print(athlete.get_profile_summary())
+
+    print("---Açığa Alınmış Sporcular---")
+    for athlete in repo.filter_by_status(AthleteStatus.Suspended):
+        print(athlete.get_profile_summary())
+
+    print("---Emekli Olmuş Sporcular---")
+    for athlete in repo.filter_by_status(AthleteStatus.Retired):
+        print(athlete.get_profile_summary())
+
+
+
+
